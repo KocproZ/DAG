db1aa415b20a615240f21c02c4ef194cc82955df
KocproZ <kocproz@protonmail.com>
2018-08-14 13:20:12 +0200

    Working nice

--- src/ovh/kocproz/dag/DAG.java
+++ src/ovh/kocproz/dag/DAG.java
@@ -11,7 +11,7 @@ import java.util.Queue;
  */
 public class DAG {
 
-    private List<Node> nodes;
+    List<Node> nodes;
     Queue<Node> output;
 
     public DAG() {
@@ -23,4 +23,59 @@ public class DAG {
         nodes.add(node);
     }
 
+    void generateGraph() {
+        List<Node> cycleCrawlerPath = new ArrayList<>();
+        for (Node n : nodes) {
+            checkForCycles(n, cycleCrawlerPath);
+        }
+
+        List<Node> tmpNodes = new ArrayList<Node>(nodes);
+        List<Node> toRemove = new ArrayList<Node>();
+
+        for (Node n : nodes)
+            if (n.getDependencies().size() == 0) {
+                output.add(n);
+                tmpNodes.remove(n);
+            }
+
+        while (nodes.size() != output.size()) {
+            for (Node n : tmpNodes) {
+                if (output.containsAll(n.getDependencies())) {
+                    output.add(n);
+                    toRemove.add(n);
+                }
+            }
+            tmpNodes.removeAll(toRemove);
+            toRemove.clear();
+        }
+    }
+
+    private void checkForCycles(Node n, List<Node> path) {
+        if (path.contains(n)) {
+            path.add(n);
+            throw new CycleFoundException(getPath(path.subList(path.indexOf(n), path.size())));
+        }
+        path.add(n);
+        n.getDependencies().forEach(node -> {
+            checkForCycles(node, path);
+        });
+        path.remove(path.size() - 1);
+    }
+
+    String getPath(List<Node> path) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < path.size() - 1; i++) {
+            sb.append(path.get(i).getObject().toString());
+            sb.append("->");
+        }
+        sb.append(path.get(path.size() - 1).getObject().toString());
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "DAG{" +
+                "nodes=" + nodes.size() +
+                '}';
+    }
 }
--- src/ovh/kocproz/dag/Main.java
+++ src/ovh/kocproz/dag/Main.java
@@ -1,24 +1,42 @@
 package ovh.kocproz.dag;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         DAG dag = new DAG();
 
-        Node n1 = new Node("Node1");
-        Node n2 = new Node("Node2");
-        Node n3 = new Node("Node3");
-        n3.addDependency(n2);
-        Node n4 = new Node("Node4");
-        Node n5 = new Node("Node5");
-        n5.addDependencies(n3, n4);
+        Random r = new Random();
+        Node n = new Node("Node0");
+        for (int i = 1; i < 10001; i++) {
+            dag.insertNode(n);
+            n = new Node("Node" + i);
+            n.addDependency(dag.nodes.get(r.nextInt(i)));
+        }
 
-        dag.insertNode(n1);
-        dag.insertNode(n2);
-        dag.insertNode(n3);
-        dag.insertNode(n4);
-        dag.insertNode(n5);
 
+        long startTime = System.currentTimeMillis();
+        dag.generateGraph();
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Graph with " + dag.nodes.size() + " nodes generated in " + (endTime - startTime) + " miliseconds.");
+
+//        printNodes(dag);
+//        printQueue(dag);
     }
+
+    static void printQueue(DAG dag) {
+        System.out.println("=== Output Queue ===");
+        for (Node n : dag.output)
+            System.out.println(n.toString());
+    }
+
+    static void printNodes(DAG dag) {
+        System.out.println("=== List on Nodes ===");
+        for (Node n : dag.nodes)
+            System.out.println(n.toString());
+    }
+
 }
